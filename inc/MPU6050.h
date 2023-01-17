384c6ec618dfd4de207249ff47360f31a65489cb
Astranot <kozha.akhmt@gmail.com>
2023-01-17 23:42:23 +0300

    checkpoint

--- inc/MPU6050.h
+++ inc/MPU6050.h
@@ -6,6 +6,32 @@
 #define updateTimes 10
 static int addr = 0x68;
 
+void mpu6050_reset() {
+uint8_t buf[] = {0x6B, 0x00};
+i2c_write_blocking(i2c1, addr, buf, 2, false);
+}
+
+void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+uint8_t buffer[6];
+uint8_t val = 0x3B;
+i2c_write_blocking(i2c1, addr, &val, sizeof(val), false); 
+i2c_read_blocking(i2c1, addr, buffer, sizeof(buffer), false);
+  for (int i = 0; i < 3; i++) {
+    accel[i] = int16_t  (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+  }
+val = 0x43;
+i2c_write_blocking(i2c1, addr, &val, sizeof(val), false); 
+i2c_read_blocking(i2c1, addr, buffer, sizeof(buffer), false);
+  for (int i = 0; i < 3; i++) {
+    gyro[i] = int16_t  (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+  }
+val = 0x41;
+i2c_write_blocking(i2c1, addr, &val, sizeof(val), false); 
+i2c_read_blocking(i2c1, addr, buffer, sizeof(buffer), false);
+
+*temp =  int16_t (buffer[0] << 8 | buffer[1]);
+}
+
 class MPU6050{
 public:
     int16_t acceleration[3], gyroscope[3],temp;
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "Servo.h"
 #include "Motions.h"
 //#include "NRF24.h"
+#include "hardware/i2c.h"
+#include "MPU6050.h"
 
 void CreepingGait(float direction, float stepLength) {
 
@@ -21,23 +23,28 @@ void CreepingGait(float direction, float stepLength) {
     bool isFinished = false;
     float freq = 0;
     float dist = 30.0;
+    Eigen::Vector3f tmp_vec;
     while (true) {
         switch (current) {
             case (first) :
                 isFinished = leg[2].stepCycle(-direction, stepLength, freq);
-                moveBody(Eigen::Vector3f(dist, -dist, 60));
+                // tmp_vec = Eigen::Vector3f(dist, -dist, 60);
+                // moveBody(&tmp_vec);
                 break;
             case (second):
                 isFinished = leg[3].stepCycle(-direction, stepLength, freq);
-                moveBody(Eigen::Vector3f(-dist, -dist, 60));
+                // tmp_vec = Eigen::Vector3f(-dist, -dist, 60);
+                // moveBody(&tmp_vec);
                 break;
             case (third):
                 isFinished = leg[0].stepCycle(direction, stepLength, freq);
-                moveBody(Eigen::Vector3f(dist, dist, 60));
+                // tmp_vec = Eigen::Vector3f(dist, dist, 60);
+                // moveBody(&tmp_vec);
                 break;
             case (fourth):
                 isFinished = leg[1].stepCycle(direction, stepLength, freq);
-                moveBody(Eigen::Vector3f(-dist, dist, 60));
+                // tmp_vec = Eigen::Vector3f(-dist, dist, 60);
+                // moveBody(&tmp_vec);
                 break;
         }
 
@@ -103,26 +110,25 @@ int main()                                                          //Main Funct
     sleep_ms(500);
     //multicore_launch_core1(walkCycle); todo reorganize multicore
 
-    //MPUTest();
+    MPUTest();
     //NRFTest();
-    defineServo();
+    // defineServo();
 
-    float direction = PI / 2;
-    float stepLength = 100;
-    //while (true) {
-        //walk();
-        //test();
-        //leg[1].slide(80,40,60);
-        //walkCycle();
-        //rotationCycle(1);
-        //walkCycle();
-        //printf("Leg1`s al: %f ,  bet: %f ,  gam: %f  \n", leg[0].lastAng(1), leg[0].lastAng(2), leg[0].lastAng(3));
-        //bodyCircularMotion();
-        //test();
-
-        CreepingGait(direction, stepLength);
-        //LizardWalk(direction, stepLength);
-    //}
+    // float direction = PI / 2;
+    // float stepLength = 100;
+    // while (true) {
+    //     //walk();
+    //     //test();
+    //     //leg[1].slide(80,40,60);
+    //     //walkCycle();
+    //     //rotationCycle(1);
+    //     //walkCycle();
+    //     //printf("Leg1`s al: %f ,  bet: %f ,  gam: %f  \n", leg[0].lastAng(1), leg[0].lastAng(2), leg[0].lastAng(3));
+    //     //bodyCircularMotion();
+    //     //test();
+    //     //CreepingGait(direction, stepLength);
+    //     //LizardWalk(direction, stepLength);
+    // }
 }
 
 /*
@@ -148,90 +154,91 @@ void NRFTest() {
 
     }
 }*/
-//
-//void MPUTest(){
-//    //todo merge MPU6050 lib with new roll, pitch calculation functions.
-//    stdio_init_all();
-//
-//    i2c_init(i2c1, 100 * 1000);
-//    gpio_set_function(15, GPIO_FUNC_I2C);
-//    gpio_set_function(14, GPIO_FUNC_I2C);
-//    gpio_pull_up(15);
-//    gpio_pull_up(14);
-//
-//    mpu6050_reset();
-//
-//    int16_t acceleration[3], gyro[3], temp;
-//
-////    defineServo();
-////    defaultPos();
-//
-//    float offset = 20;
-//
-//    double past;
-//    double integral;
-//    double intConst = 0.00001;
-//
-//    double present;
-//    double error;
-//    double errorConst = 0.5;
-//
-//    double future;
-//    double derivative;
-//    double last;
-//    double now;
-//    double derConst = 0.001;
-//
-//
-//    absolute_time_t lastTime;
-//    long double errorSum;
-//    float bound = 0.2;
-//    int skip = 0;
-//
-//    while (1) {
-//
-//        roll =  atanf( - acceleration[0] / sqrtf(acceleration[1]*acceleration[1] + acceleration[2]*acceleration[2]));
-//        pitch = (atanf( - acceleration[1] / sqrtf(acceleration[0]*acceleration[0] + acceleration[2]*acceleration[2])));
-//
-//        error = ( 0 - roll ) ;
-//        present = errorConst * error;
-//
-//        integral += error;
-//        past = intConst * integral;
-//
-//
-//        roll =  atanf( - acceleration[0] / sqrtf(acceleration[1]*acceleration[1] + acceleration[2]*acceleration[2]));
-////        rollAcc = atanf( - gyro[0] / sqrtf(acceleration[1]*acceleration[1] + acceleration[2]*acceleration[2]));
-//
-////        derivative = ( last - now ) / (double) absolute_time_diff_us(lastTime,get_absolute_time());
-//        derivative = gyro[0];
-//        future = derConst * derivative;
-//
-//        errorSum = present  ;
-//
-//
-//
-//        mpu6050_read_raw(acceleration, gyro, &temp);
-//        //printf("Acc. X = %6.2d, Y = %6.2d, Z = %6.2d ", acceleration[0], acceleration[1],acceleration[2]);
-//        //printf("Angles. Roll = %6.2f, Pitch = %6.2f\n",roll , pitch);
-//        mpu6050_reset();
-//
-//
-//        printf("Error sum: %.3Lf Future: %.3f Present: %.3f Past: %.3f \n", errorSum , future, present, past );
-////        for (int i = 0; i <= 3; ++i) {
-////            float y = leg[i].lastPos.y,
-////                    z = leg[i].lastPos.z;
-////            //todo use PID intead of bound;
-////            leg[i].toPos( Vector(60, 60, offset + 60 /
-////            //tanf( TanjentWithBound( roll, i, 0.5 ) ) )
-////            tanf(PI / 3 + (errorSum) * powf(-1 , i)) )
-////            );
-////        }
-//        sleep_ms(150 );
-//    }
-//
-//
-//}
+
+void MPUTest(){
+    MPU6050 mpu6050;
+   //todo merge MPU6050 lib with new roll, pitch calculation functions.
+   stdio_init_all();
+
+   i2c_init(i2c1, 100 * 1000);
+   gpio_set_function(15, GPIO_FUNC_I2C);
+   gpio_set_function(14, GPIO_FUNC_I2C);
+   gpio_pull_up(15);
+   gpio_pull_up(14);
+
+   mpu6050_reset();
+
+   int16_t acceleration[3], gyro[3], temp;
+
+//    defineServo();
+//    defaultPos();
+
+   float offset = 20;
+
+   double past;
+   double integral;
+   double intConst = 0.00001;
+
+   double present;
+   double error;
+   double errorConst = 0.5;
+
+   double future;
+   double derivative;
+   double last;
+   double now;
+   double derConst = 0.001;
+
+
+   absolute_time_t lastTime;
+   long double errorSum;
+   float bound = 0.2;
+   int skip = 0;
+
+   while (1) {
+
+       roll =  atanf( - acceleration[0] / sqrtf(acceleration[1]*acceleration[1] + acceleration[2]*acceleration[2]));
+       pitch = (atanf( - acceleration[1] / sqrtf(acceleration[0]*acceleration[0] + acceleration[2]*acceleration[2])));
+
+       error = ( 0 - roll ) ;
+       present = errorConst * error;
+
+       integral += error;
+       past = intConst * integral;
+
+
+       roll =  atanf( - acceleration[0] / sqrtf(acceleration[1]*acceleration[1] + acceleration[2]*acceleration[2]));
+//        rollAcc = atanf( - gyro[0] / sqrtf(acceleration[1]*acceleration[1] + acceleration[2]*acceleration[2]));
+
+//        derivative = ( last - now ) / (double) absolute_time_diff_us(lastTime,get_absolute_time());
+       derivative = gyro[0];
+       future = derConst * derivative;
+
+       errorSum = present  ;
+
+
+
+       mpu6050_read_raw(acceleration, gyro, &temp);
+       //printf("Acc. X = %6.2d, Y = %6.2d, Z = %6.2d ", acceleration[0], acceleration[1],acceleration[2]);
+       //printf("Angles. Roll = %6.2f, Pitch = %6.2f\n",roll , pitch);
+       mpu6050_reset();
+
+
+       printf("Error sum: %.3Lf Future: %.3f Present: %.3f Past: %.3f \n", errorSum , future, present, past );
+//        for (int i = 0; i <= 3; ++i) {
+//            float y = leg[i].lastPos.y,
+//                    z = leg[i].lastPos.z;
+//            //todo use PID intead of bound;
+//            leg[i].toPos( Vector(60, 60, offset + 60 /
+//            //tanf( TanjentWithBound( roll, i, 0.5 ) ) )
+//            tanf(PI / 3 + (errorSum) * powf(-1 , i)) )
+//            );
+//        }
+       sleep_ms(150 );
+   }
+
+
+}
 ////float TanjentWithBound(float roll, int i , float bound){
 ////    float al = PI / 3 + roll * powf(-1 , i);
 ////    if(tanf(al) - bound <= tanf(al) && tanf(al) <= tanf(al) + bound )
